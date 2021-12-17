b162da428ea2d27a5f33e13e8175b59c2a23963b
Demopans <gao.jeff3@gmail.com>
2021-12-17 12:14:13 -0500

    isolated set cookie code. cookie retrieval has issues

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+import genshinstats as gs
+import json as js
 
 def autoCheckIn():
     """
     attempt to auto check in upon starting program,
     """
-    import genshinstats as gs
-    gs.set_cookie_auto()
-    reward = gs.claim_daily_reward()
-    if reward is not None:
-        print(f"Claimed daily reward - {reward['cnt']}x {reward['name']}")
-    else:
-        print("Could not claim daily reward")
+    try:
+        if reward := gs.claim_daily_reward() is not None:
+            print(f"Claimed daily reward - {reward['cnt']}x {reward['name']}")
+        else:
+            print("Could not claim daily reward")
+    except Exception as e:
+        print(e)
 
 
 def tmp():
     import schedule
     import time
-    schedule.every().day.at('17:00').do(autoCheckIn)
+    schedule.every().day.at('00:00').do(autoCheckIn)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
+def configCookie():
+    t = gs.get_browser_cookies()
+    if not t:  # failed to get cookies
+        # read cookies from config file
+        ltuid, ltoken = input("ltuid: "), input("ltoken: ")
+        t = {'ltuid':ltuid, 'ltoken':ltoken}
+    gs.set_cookie(t)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    configCookie()
     autoCheckIn()
     tmp()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
