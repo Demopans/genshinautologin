997b5121da1dd33584f05305d2f5574e3690e925
Demopans <gao.jeff3@gmail.com>
2022-05-15 11:29:09 -0400

    swapped to nuitka build system
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- genshinautocheckin/main.py
+++ genshinautocheckin/main.py
@@ -1,49 +1,82 @@
-import genshinstats as gs
+import asyncio
+import datetime
+from typing import TextIO, Mapping, BinaryIO, Any, Coroutine
+from cryptography.fernet import Fernet
+import os.path
+import genshin.client
+import genshin.utility
+import asyncio
 import sys
 
 
-def autoCheckIn() -> None:
-    """
-    attempt to auto check in upon starting program,
-    """
-    try:
-        reward = gs.claim_daily_reward()
-        if reward is not None:
-            print(f"Claimed daily reward - {reward['cnt']}x {reward['name']}")
-        else:
-            print("Could not claim daily reward")
-    except Exception as e:
-        print(e)
-
-
-def tmp() -> None:
-    import schedule
-    import time
-    schedule.every().day.at('00:00').do(autoCheckIn)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
-def configCookie() -> None:
-    args: list[str] = sys.argv
-
-    cookies = {}
-
-    if len(args) != 3:
-        print('Usage: main.exe <ltuid> <ltoken>')
-        cookies = gs.get_browser_cookies()
-    else:
-        cookies = {'ltuid': args[1], 'ltoken': args[2]}
-
-    gs.set_cookies(cookies)
-
-
-def main():
-    configCookie()
-    autoCheckIn()
-    tmp()
-
-
 if __name__ == '__main__':
-    main()
+    # do on startup
+    def configCookie(c: genshin.client.Client, log: TextIO, override=False) -> None:
+        usr = []
+        def writeData(strr: str) -> None:
+            data: BinaryIO = open("./data", "ab+")
+
+            key: bytes = Fernet.generate_key()
+            with open("./key", "wb") as key_file:
+                key_file.write(key)
+                key_file.close()
+
+            f = Fernet(key)
+            enc = f.encrypt(strr.encode())
+            data.write(enc+'\n'.encode())
+            data.close()
+
+        def readFromData():
+            key = open("./key", "rb").read()
+            data = open("./data", "rb+").read()
+            f = Fernet(key)
+            t = f.decrypt(data).decode().split("\n")
+            for i in t:
+                tt = i.split()
+                usr.append({"ltuid": tt[0], "ltoken": tt[1]})
+
+        dataExists = os.path.exists("./data") and os.path.exists("./key")
+        if override:
+            #clear file
+            data = open("./data", "wb+")
+            data.close()
+
+            writeData(open(sys.argv[1]).read())
+            readFromData()
+        else:
+            if dataExists:
+                readFromData()
+            else:
+                cookies: Mapping[str, str] = genshin.utility.get_browser_cookies()
+                if len(cookies.keys()) == 0:
+                    log.write(f"[{datetime.datetime.now()}]ERROR: Failed to get cookies")
+                    exit(-1)
+                writeData(f"{cookies['ltuid']} {cookies['ltoken']}\n")
+        c.set_cookies(usr)
+
+    async def task(c: genshin.client.Client, log):
+        """
+        attempt to auto check in upon starting program,
+        """
+        try:
+            reward = await c.claim_daily_reward()
+        except Exception:
+            log.write("Daily reward already claimed")
+        else:
+            log.write(f"Claimed {reward.amount}x\"{reward.name}\"")
+        log.flush()
+
+    def service(c: genshin.client.Client, log):
+        import schedule
+        import time
+        asyncio.run(task(c,log))
+        schedule.every().day.at('00:00').do(task, c, log)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
+    cli = genshin.client.Client()
+
+    log = open("./log.txt", 'w+')
+    configCookie(cli, log, len(sys.argv) > 1)
+    service(cli, log)
