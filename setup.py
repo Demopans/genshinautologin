b7a2b3a1e5dca19710c146801bfdbd3d6f11b8e2
Demopans <gao.jeff3@gmail.com>
2022-05-31 23:31:11 -0400

    update
    Signed-off-by: Demopans <gao.jeff3@gmail.com>

--- setup.py
+++ setup.py
@@ -1,1 +1,4 @@
-#! python -m nuitka --enable-plugin=anti-bloat --onefile --standalone -o out/main.exe --output-dir=main.build genshinautocheckin/main.py
+# list of setup commands
+# pyinstaller --onefile -c genshinautocheckin/main.py
+# python -m nuitka --standalone genshinautocheckin/main.py
+# python -m nuitka --standalone --onefile -o=out/main.exe genshinautocheckin/main.py
